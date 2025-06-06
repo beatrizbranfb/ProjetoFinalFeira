4c12010f708d965145f1d40abd1ec3d4d21ef392
Renan Santos <renan.santos.oliveira.sa@gmail.com>
2025-06-05 22:26:55 -0300

    métodos e métodos de classe

--- app/models/product.py
+++ app/models/product.py
@@ -1,4 +1,4 @@
-
+from app.controllers.db import database
 
 
 class Product:
@@ -8,3 +8,54 @@ class Product:
         self.description = description
         self.price = price
         self.stock = stock
+
+    @staticmethod
+    def get_db():
+        return database.get_db()
+    
+    @classmethod
+    def get_all(cls):
+        with cls.get_db() as db:
+            cursor = db.execute("SELECT * FROM products")
+            return [cls(
+                p['id'],
+                p['name'],
+                p['description'],
+                p['price'],
+                p['stock']
+                ) 
+            for p in cursor.fetchall()]
+    
+    @classmethod
+    def find_by_id(cls, product_id):
+        with cls.get_db() as db:
+            cursor = db.execute("SELECT * FROM products WHERE id = ?", (product_id,))
+            product_data = cursor.fetchone()
+            if product_data:
+                return cls(
+                    product_data['id'],
+                    product_data['name'],
+                    product_data['description'],
+                    product_data['price'],
+                    product_data['stock']
+                )
+            return None
+        
+    @classmethod
+    def create(self):
+        with self.get_db() as db:
+            cursor = db.execute("INSERT INTO products (name, description, price, stock) VALUES (?, ?, ?, ?)",
+                                (self.name, self.description, self.price, self.stock))
+            db.commit()
+            self.id = cursor.lastrowid
+    
+    def save(self):
+        with self.get_db() as db:
+            db.execute("UPDATE products SET name=?, description=?, price=?, stock=? WHERE id=?",
+                       (self.name, self.description, self.price, self.stock, self.id))
+            db.commit()
+
+    def delete(self):
+        with self.get_db() as db:
+            db.execute("DELETE FROM products WHERE id=?", (self.id,))
+            db.commit()
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-
+from app.controllers.db import database
 
 
 class User:
@@ -8,3 +8,41 @@ class User:
         self.email = email
         self.password = password
         self.role = role
+
+    @staticmethod
+    def get_db():
+        return database.get_db()
+
+    @classmethod
+    def find_by_username(cls, username):
+        with cls.get_db() as db:
+            cursor = db.execute("SELECT * FROM users WHERE username = ?", (username,))
+            user_data = cursor.fetchone()
+            if user_data:
+                return cls(
+                    user_data['id'],
+                    user_data['username'],
+                    user_data['password'],
+                    user_data['email'],
+                    user_data['role']
+                    )
+            return None
+
+    @classmethod
+    def create(cls, username, password, email, role='customer'):
+        with cls.get_db() as db:
+            cursor = db.execute("INSERT INTO users (username, password, email, role) VALUES (?, ?, ?, ?)",
+                                (username, password, email, role))
+            db.commit()
+            return cursor.lastrowid
+
+    def save(self):
+        with self.get_db() as db:
+            db.execute("UPDATE users SET username=?, password=?, email=?, role=? WHERE id=?",
+                       (self.username, self.password, self.email, self.role, self.id))
+            db.commit()
+
+    def delete(self):
+        with self.get_db() as db:
+            db.execute("DELETE FROM users WHERE id=?", (self.id,))
+            db.commit()
